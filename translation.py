4910649446646690192e049be8bd4666d3ddc23a
madloyer <87596107+madloyer@users.noreply.github.com>
2021-09-01 20:00:31 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,9 +2,11 @@ import os
 
 class Translation(object):
   START_TXT = """<b>Yooo {}!!</b> wasaap!!
-  <code>List Channel
+  <code>List Channel</code>
   • @mantapvids
   • @mantapfilestorage
-  • @mantapjozztutorial</code>
+  • @mantapjozztutorial
   <b>Info Group VIP Cek Link dibawah</b>
-  <a href="https://t.me/mantapvids/571">Join Vip Indo</a>"""
+  <a href="https://t.me/mantapvids/571">Join Vip Indo</a>
+  <a href="https://doujindes.blogspot.com">Website</a>
+  """
