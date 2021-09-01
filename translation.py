9bb7d93859747b2b2270280df38ba0950493eebf
madloyer <87596107+madloyer@users.noreply.github.com>
2021-09-01 20:02:43 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -6,7 +6,4 @@ class Translation(object):
   • @mantapvids
   • @mantapfilestorage
   • @mantapjozztutorial
-  <b>Info Group VIP Cek Link dibawah</b>
-  <a href="https://t.me/mantapvids/571">Join Vip Indo</a>
-  <a href="https://doujindes.blogspot.com">Website</a>
   """
